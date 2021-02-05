0004f5c5ef119532ea7bf43a08bb74788dcc4f92
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-02-05 10:51:15 +0100

    apiTester

---
+++ ReactionGame.Tester/APITester.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ReactionGame.Entety;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactionGame.Tester
+{
+    [TestClass]
+    public class APITester
+    {
+        static HttpClientHandler clientHandler = new HttpClientHandler { ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; } };
+        static HttpClient client = new HttpClient(clientHandler);
+        const string serverAdtesAPI = "http://localhost:5000/Highscores";
+
+        [TestMethod]
+        public async Task TestIfICanAddAHighscoreAPIAsync()
+        {
+            //Arrange
+            Highscore highscore = new Highscore("Tester", 100);
+
+            //Act
+            HttpResponseMessage httpResponseMessage = await client.PostAsJsonAsync(serverAdtesAPI, highscore);
+
+
+            //Assert
+            Assert.IsTrue(httpResponseMessage.StatusCode == System.Net.HttpStatusCode.Created);
+        }
+    }
+}
